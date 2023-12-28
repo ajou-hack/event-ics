e3989a613f4c9ee0555a0216d0b39ce9f2cdf184
parksb <parkgds@gmail.com>
2023-12-28 21:51:09 +0900

    feat: 12월에는 내년 일정도 함께 읽도록 개선

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,24 @@ fn main() {
         .build()
         .unwrap();
 
-    let year = Utc::now().year();
-    let events = (1..13)
-        .flat_map(|month| -> Vec<Event> {
-            thread::sleep(Duration::from_millis(1000));
-            fetch_events(year, month, &client)
+    let now = Utc::now();
+    let years = if Utc::now().month() < 12 {
+        [now.year(); 2]
+    } else {
+        [now.year(), now.year() + 1]
+    };
+
+    let events = years
+        .iter()
+        .flat_map(|year| -> Vec<Event> {
+            (1..12)
+                .flat_map(|month| -> Vec<Event> {
+                    thread::sleep(Duration::from_millis(1000));
+                    fetch_events(*year, month, &client)
+                })
+                .unique_by(|event| event.article_no)
+                .collect::<Vec<Event>>()
         })
-        .unique_by(|event| event.article_no)
         .collect::<Vec<Event>>();
 
     let result = compose_ical(&events);
@@ -49,7 +60,7 @@ fn fetch_events(year: i32, month: i32, client: &Client) -> Vec<Event> {
     let url = format!("{}?{}", base_url, params);
 
     let response = client
-        .get(&url)
+        .get(url)
         .header("User-Agent", "Mozilla/5.0")
         .header("Connection", "keep-alive")
         .header("Cache-Control", "no-cache")
@@ -63,7 +74,7 @@ fn fetch_events(year: i32, month: i32, client: &Client) -> Vec<Event> {
 }
 
 fn compose_ical(events: &[Event]) -> String {
-    let header = r#"BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Ajou University/학사일정//KR\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH"#;
+    let header = r"BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Ajou University/학사일정//KR\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH";
 
     let items = events
         .iter()
@@ -72,15 +83,15 @@ fn compose_ical(events: &[Event]) -> String {
                 r#"BEGIN:VEVENT\nUID:{}\nSUMMARY:{}\nDTSTART:{}\nDTEND:{}\nDTSTAMP:{}\nEND:VEVENT"#,
                 Uuid::new_v4(),
                 event.title,
-                event.start.replace("-", ""),
-                event.end.replace("-", ""),
+                event.start.replace('-', ""),
+                event.end.replace('-', ""),
                 Utc::now().format("%Y%m%dT%H%M%SZ"),
             )
             .trim()
             .to_string()
         })
         .collect::<Vec<String>>()
-        .join(r#"\n"#);
+        .join(r"\n");
 
     let footer = r#"END:VCALENDAR"#;
 
